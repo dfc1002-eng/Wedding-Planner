80e66178c1d62d858f73ba766f31f50f04ee8f37
[dyad] <contato@rjweddings.com.br>
2025-11-14 13:58:43 -0300

    [dyad] wrote 1 file(s)

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -31,7 +31,7 @@ const Tooltip: React.FC<TooltipProps> = ({ text, children, position = 'top', dis
 
   return (
     <div 
-      className="relative flex w-full"
+      className="relative flex w-full h-full" // Adicionado h-full aqui
       onMouseEnter={() => setIsVisible(true)}
       onMouseLeave={() => setIsVisible(false)}
       onFocus={() => setIsVisible(true)}
